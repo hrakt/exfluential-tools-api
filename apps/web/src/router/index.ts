7fc9e9e99c25135667c8ac6b813394cb0b8f4cff
Hrak Torousian <hraktorres@gmail.com>
2026-01-06 20:27:16 -0800

    fix routing for frontend

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import RequestAssetView from '../views/RequestAssetView.vue'
+import RequestAssetView from '@/views/RequestAssetView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
